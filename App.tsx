a9311f291fb2d532b4dfc2ae8744a61946ea60d5
Luca Vaccaro <me@lvaccaro.com>
2024-12-14 18:01:14 +0100

    updates
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- App.tsx
+++ App.tsx
@@ -25,6 +25,9 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import { useState, useEffect } from 'react';
+import { Wollet, Client, Signer, Network } from 'lwk-rn';
+
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
@@ -58,61 +61,31 @@ function Section({children, title}: SectionProps): React.JSX.Element {
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
+  const [result, setResult] = useState<string | undefined>();
+
+  useEffect(() => {
+    async function setup() {
+      const mnemonic =
+        'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about';
+      console.log(mnemonic)
+      const network = Network.Testnet;
+      const signer = await new Signer().create(mnemonic, network);
+      const descriptor = await signer.wpkhSlip77Descriptor();
+      let txt = await descriptor.asString();
+      console.log(txt);
+      setResult(txt);
+    }
+    setup();
+  }, []);
+
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={backgroundStyle}>
+      <Text>Descriptor: {result}</Text>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
